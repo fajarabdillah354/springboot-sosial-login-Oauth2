daea236639fb2678f65f4c86011fe7375e849305
fajar abdillah <fajardillah25@gmail.com>
2025-02-03 12:19:38 +0700

    Upload

---
+++ src/main/java/dev/codejar/springboot_sosial_login_with_Oauth2/Application.java
@@ -0,0 +1,13 @@
+package dev.codejar.springboot_sosial_login_with_Oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
