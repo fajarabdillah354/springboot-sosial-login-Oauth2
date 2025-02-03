fd050b558d6f1e96f87288ef3090cb41ae0affed
fajar abdillah <fajardillah25@gmail.com>
2025-02-03 12:18:59 +0700

    Upload

---
+++ src/main/java/dev/codejar/springboot_sosial_login_with_Oauth2/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package dev.codejar.springboot_sosial_login_with_Oauth2.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
